b8bee03513e4c2dc3f4ca174c17630ac8164345a
Emirhan EMMEZ <emirhanemmez@gmail.com>
2021-12-10 10:47:55 +0300

    Add download and static methods

--- src/main/kotlin/com/emirhanemmez/plugins/Routing.kt
+++ src/main/kotlin/com/emirhanemmez/plugins/Routing.kt
@@ -2,9 +2,11 @@ package com.emirhanemmez.plugins
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     val userList = mutableListOf("emirhan", "ahmet", "mehmet", "zeynep")
@@ -45,12 +47,47 @@ fun Application.configureRouting() {
                         userList.removeAt(index)
                         call.respond(status = HttpStatusCode.OK, userList)
                     } else {
-                        call.respondText(status = HttpStatusCode.NotFound, text = "The person in ${index}. index does not exist")
+                        call.respondText(
+                            status = HttpStatusCode.NotFound,
+                            text = "The person in ${index}. index does not exist"
+                        )
                     }
                 } else {
                     call.respondText(status = HttpStatusCode.BadRequest, text = "Wrong index entered!")
                 }
             }
+        }
+        
+        static("/image") {
+            defaultResource("kedi.jpg", "files")
+        }
+
+        get("/downloadImageAsAttachment") {
+
+            val file = File("src/main/resources/files/kedi.jpg")
+
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(
+                    ContentDisposition.Parameters.FileName,
+                    "downloadedImage.jpg"
+                ).toString()
+            )
+
+            call.respondFile(file)
+        }
+
+        get("/downloadImageAsInline") {
+            val file = File("src/main/resources/files/kedi.jpg")
+
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Inline.withParameter(
+                    ContentDisposition.Parameters.FileName, "downloadedKediImage.jpg"
+                ).toString()
+            )
+
+            call.respondFile(file)
         }
     }
 }
